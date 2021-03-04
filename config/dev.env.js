7fe8181afe9d4c1d310fcea3d10e55472fe7050d
Mery <maria.juan.vi@gmail.com>
2021-03-04 14:22:24 +0100

    Arreglo

--- config/dev.env.js
+++ config/dev.env.js
@@ -5,5 +5,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  TOKEN: '"4a761a6626d109ed303199acbd0ac24664cc770c"'
+  TOKEN: '"f2f8d5fec1164868ca82264f4bdd46ec9a2cb123"'
 })
